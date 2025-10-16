2b3fa637ca94c0a503fd81be50d06a6a457f90bb
Patrik <PatrikLazsloKelemen@gmail.com>
2025-10-16 02:17:37 -0400

    slight cleanup

--- synth2.cpp
+++ synth2.cpp
@@ -30,6 +30,7 @@ protected: // Sound Specific
         std::string label;
         int waveformIndex;
         olc::vi2d position;
+		bool white;
     };
 
     std::vector<KeyButton> keyButtons;
@@ -62,6 +63,16 @@ public:
 				y *=  1+16*t*exp(-12*t);
 				//y *=  1+16*t*exp(-12*t);
 				//y *= 1- ((double) rand() / (RAND_MAX))/(4*exp(-3*(t-1))) ;
+				
+				
+				//square wave test 
+				if(sin(1 * freq * 3.14159 * t) >.9){
+					y=0.5* exp(-6 *t);
+				}
+				else {
+					y=-0.5* exp(-6 *t);
+				}
+				
 				
 				y *= std::min(1.0,exp(-80*(t-0.97))); //reset end position to zero
 				y *= std::min(1.0,exp(300*(t-0.01))); //force start position to come in slower 
@@ -113,30 +124,30 @@ public:
 
 		
 		keyButtons = {
-            {olc::Key::E, "E", 3, {20, 60}},
-            {olc::Key::K4, "4", 4, {40, 20}},
-            {olc::Key::R, "R", 5, {60, 60}},
-            {olc::Key::K5, "5", 6, {80, 20}},
-            {olc::Key::T, "T", 7, {100, 60}},
-            {olc::Key::S, "S", 8, {140, 60}},
-            {olc::Key::X, "X", 9, {160, 20}},
-            {olc::Key::D, "D",10, {180, 60}},
-            {olc::Key::C, "C",11, {200, 20}},
-            {olc::Key::F, "F",12, {220, 60}},
-            {olc::Key::V, "V",13, {240, 20}},
-            {olc::Key::G, "G",14, {260, 60}},
-            {olc::Key::Y, "Y",15, {20, 140}},
-            {olc::Key::K7, "7",16, {40, 100}},
-            {olc::Key::U, "U",17, {60, 140}},
-            {olc::Key::K8, "8",18, {80, 100}},
-            {olc::Key::I, "I",19, {100, 140}},
-            {olc::Key::H, "H",20, {140, 140}},
-            {olc::Key::N, "N",21, {160, 100}},
-            {olc::Key::J, "J",22, {180, 140}},
-            {olc::Key::M, "M",23, {200, 100}},
-            {olc::Key::K, "K",24, {220, 140}},
-            {olc::Key::COMMA, ",",25,{240, 100}},
-            {olc::Key::L, "L",26, {260, 140}}
+            {olc::Key::E, "E", 3, {20, 60},true},
+            {olc::Key::K4, "4", 4, {40, 20},false},
+            {olc::Key::R, "R", 5, {60, 60},true},
+            {olc::Key::K5, "5", 6, {80, 20},false},
+            {olc::Key::T, "T", 7, {100, 60},true},
+            {olc::Key::S, "S", 8, {140, 60},true},
+            {olc::Key::X, "X", 9, {160, 20},false},
+            {olc::Key::D, "D",10, {180, 60},true},
+            {olc::Key::C, "C",11, {200, 20},false},
+            {olc::Key::F, "F",12, {220, 60},true},
+            {olc::Key::V, "V",13, {240, 20},false},
+            {olc::Key::G, "G",14, {260, 60},true},
+            {olc::Key::Y, "Y",15, {20, 140},true},
+            {olc::Key::K7, "7",16, {40, 100},false},
+            {olc::Key::U, "U",17, {60, 140},true},
+            {olc::Key::K8, "8",18, {80, 100},false},
+            {olc::Key::I, "I",19, {100, 140},true},
+            {olc::Key::H, "H",20, {140, 140},true},
+            {olc::Key::N, "N",21, {160, 100},false},
+            {olc::Key::J, "J",22, {180, 140},true},
+            {olc::Key::M, "M",23, {200, 100},false},
+            {olc::Key::K, "K",24, {220, 140},true},
+            {olc::Key::COMMA, ",",25,{240, 100},false},
+            {olc::Key::L, "L",26, {260, 140},true}
         };
 		
 		return true;
@@ -180,7 +191,7 @@ public:
 
             // Optional: Play waveform on press (like your original logic)
             if (GetKey(btn.key).bPressed) {
-                engine.PlayWaveform(&wavelist[btn.waveformIndex]);  // <-- You must define 'engine' and 'wavelist'
+                engine.PlayWaveform(&wavelist[btn.waveformIndex]);  
             }
         }
 		
@@ -194,6 +205,10 @@ public:
 				loopcounter=0;
 			}
 		} */
+		
+		
+		//draw waveform 
+		
 		
 		
 		DrawString({ 4,  4 }, "[1] Play 44100Hz x 8-bit Custom Sample");		
